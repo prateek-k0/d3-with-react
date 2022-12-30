d0041544b425adc61704b66bffba699d5c67f65f
prateek-k1 <parx1601@gmail.com>
2022-12-30 21:44:46 +0530

    404 component changes

--- src/Components/404Component/index.js
+++ src/Components/404Component/index.js
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 import { Button, Typography } from "@mui/material";
 import '@fontsource/abeezee/400.css';
 import error404Image from './404-image.png';
+import { useSelector } from "react-redux";
 
 const Default404Component = () => {
+    const isDarkMode = useSelector(state => state.theme.darkMode);
     return (
         <Box sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '80px 0 40px'}}>
             <img src={error404Image} alt='404-error' width={600}/>
-            <Typography sx={{fontSize: '24px', fontFamily: '"ABeeZee", sans-serif', margin: '80px 0 40px'}}>
+            <Typography sx={{fontSize: '24px', fontFamily: '"ABeeZee", sans-serif', margin: '80px 0 40px', textAlign: 'center'}}>
                 It is recommended to use the sidebar for navigating through the graphs.
             </Typography>
-            <Button variant="outlined" size="large" sx={{padding: '12px 60px', fontFamily: '"ABeeZee", sans-serif'}} component={Link} to="/">
+            <Button variant={isDarkMode ? 'outlined' : 'contained'} size="large" sx={{padding: '12px 60px', fontFamily: '"ABeeZee", sans-serif'}} component={Link} to="/">
                 Homepage
             </Button>
         </Box>
