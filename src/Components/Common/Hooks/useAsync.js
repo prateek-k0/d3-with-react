9c2b34cc277442ad2ceac7e485d71c50e4ddb1a3
prateek-k1 <parx1601@gmail.com>
2022-12-31 12:27:20 +0530

    useAsync hook added

---
+++ src/Components/Common/Hooks/useAsync.js
@@ -0,0 +1,36 @@
+import { useState, useLayoutEffect, useCallback } from 'react';
+
+const useAsync = (callback, immediate = true) => {
+    const [status, setStatus] = useState('idle');
+    const [value, setValue] = useState(null);
+    const [error, setError] = useState(null);
+
+    const execute = useCallback(() => {
+        setStatus('pending');
+        setValue(null);
+        setError(null);
+        const isAsyncFunction = callback[Symbol.toStringTag] === 'AsyncFunction';
+        if (isAsyncFunction) {
+            callback()
+            .then((result) => {
+                setValue(result);
+                setStatus('fulfilled');
+            }).catch((err) => {
+                setError(err);
+                setStatus('rejected');
+            });
+        } else {
+            throw new Error('Callback is not an async function. useAync only works with async functions.');
+        }
+    }, [callback]);
+
+    // Call execute if we want to fire it right away.
+    // Otherwise execute can be called later, such as in an onClick handler.
+    useLayoutEffect(() => {
+        immediate && execute();
+    }, [execute, immediate]);
+
+    return { execute, status, value, error }
+}
+
+export { useAsync };
--- src/Components/Graphs/maps/choropleth-map/ChoroplethMapComponent.js
+++ src/Components/Graphs/maps/choropleth-map/ChoroplethMapComponent.js
@@ -1,108 +1,123 @@
 import React, { useCallback } from 'react';
 import { useD3 } from '../../../Common/Hooks/useD3';
+import { useAsync } from '../../../Common/Hooks/useAsync';
 import * as d3 from 'd3';
 import '@fontsource/space-mono/400.css';
 import '@fontsource/abeezee/400.css'
 import { Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
+import LoadingComp from '../../../Common/LoadingComponent/LoadingComponent';
+import ErrorComp from '../../../Common/ErrorComponent/ErrorComponent';
 
 const ChoroplethMap = () => {
     const isDarkMode = useSelector(state => state.theme.darkMode);
 
+    const geoDataUrl = 'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson';
+    const popDataUrl = 'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv';
+
+    const getGeoDataCallback = useCallback(async () => {
+        return d3.json(geoDataUrl);
+    }, []);
+    const getPopDataCallback = useCallback(async () => {
+        return d3.csv(popDataUrl);
+    }, []);
+
+    const { value: geoData, status: geoDataStatus } = useAsync(getGeoDataCallback, true);
+    const { value: popData, status: popDataStatus } = useAsync(getPopDataCallback, true);
+
     const renderFunc = useCallback(async (container) => {
-        const width = container.node().clientWidth, height = 1500;
-        const svg = container
-            .append("svg")
-            .attr("width", '100%')
-            .attr("height", height);
+        if(geoDataStatus === 'fulfilled' && popDataStatus === 'fulfilled') {
+            const width = container.node().clientWidth, height = 1500;
+            const svg = container
+                .append("svg")
+                .attr("width", '100%')
+                .attr("height", height);
 
-        // const path = d3.geoPath();
-        const projection = d3.geoMercator()
-              .scale(width / 7)
-              .center([0, 0])
-              .rotate([-12, 0])
-              .translate([width / 2, height / 2]);
+            // const path = d3.geoPath();
+            const projection = d3.geoMercator()
+                .scale(width / 7)
+                .center([0, 0])
+                .rotate([-12, 0])
+                .translate([width / 2, height / 2]);
 
-        const colorScale = d3.scaleThreshold()
-            .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-            .range(d3.schemeBlues[7]);
+            const colorScale = d3.scaleThreshold()
+                .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+                .range(d3.schemeBlues[7]);
 
-        const data = new Map();
+            const data = new Map();
 
-        const geoData = await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson");
-        const popData = await d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv");
-        popData.forEach((d) => {
-            data.set(d.code, +d.pop);
-        });
+            popData.forEach((d) => {
+                data.set(d.code, +d.pop);
+            });
 
-        const tooltip = container
-            .append("div")
-            .style('display', 'none')
-            .attr("class", "tooltip")
-            .style("background-color", isDarkMode ? '#000' : '#fff')
-            .style("border", "solid")
-            .style("border-width", "2px")
-            .style("border-radius", "5px")
-            .style("padding", "5px")
-            .style('position', 'absolute')
-            .style('color', isDarkMode ? '#fff' : '#000');
-
-        let mouseOver = function(event) {
-            tooltip
-                .style('display', 'block')
-            d3.selectAll(".Country")
-                .transition()
-                .duration(300)
-                .style("opacity", 0.5)
-            d3.select(this)
-                .transition()
-                .duration(0)
-                .style("opacity", 1)
-                .style("stroke", isDarkMode ? '#fff' : '#000')
-        }
-
-        let mouseMove = function(event, d) {
-            tooltip
-                .html(d.properties.name + ":<br />" + data.get(d.id) || 0)
-                .style("left", ((event.x) - (tooltip.node().clientWidth / 2)) + "px")
-                .style("top", (event.y) + 20 + "px")
-        }
-
-        let mouseLeave = function(d) {
-            tooltip
+            const tooltip = container
+                .append("div")
                 .style('display', 'none')
-            d3.selectAll(".Country")
-                .transition()
-                .duration(0)
-                .style("opacity", 1)
-            d3.select(this)
-                .transition()
-                .duration(0)
+                .attr("class", "tooltip")
+                .style("background-color", isDarkMode ? '#000' : '#fff')
+                .style("border", "solid")
+                .style("border-width", "2px")
+                .style("border-radius", "5px")
+                .style("padding", "5px")
+                .style('position', 'absolute')
+                .style('color', isDarkMode ? '#fff' : '#000');
+
+            let mouseOver = function(event) {
+                tooltip
+                    .style('display', 'block')
+                d3.selectAll(".Country")
+                    .transition()
+                    .duration(300)
+                    .style("opacity", 0.5)
+                d3.select(this)
+                    .transition()
+                    .duration(0)
+                    .style("opacity", 1)
+                    .style("stroke", isDarkMode ? '#fff' : '#000')
+            }
+
+            let mouseMove = function(event, d) {
+                tooltip
+                    .html(d.properties.name + ":<br />" + data.get(d.id) || 0)
+                    .style("left", ((event.x) - (tooltip.node().clientWidth / 2)) + "px")
+                    .style("top", (event.y) + 20 + "px")
+            }
+
+            let mouseLeave = function(d) {
+                tooltip
+                    .style('display', 'none')
+                d3.selectAll(".Country")
+                    .transition()
+                    .duration(0)
+                    .style("opacity", 1)
+                d3.select(this)
+                    .transition()
+                    .duration(0)
+                    .style("stroke", "transparent")
+            }
+
+            svg.append("g")
+                .selectAll("path")
+                .data(geoData.features)
+                .enter()
+                .append("path")
+                // draw each country
+                .attr("d", d3.geoPath()
+                    .projection(projection)
+                )
+                // set the color of each country
+                .attr("fill", function (d) {
+                    d.total = data.get(d.id) || 0;
+                    return colorScale(d.total);
+                })
                 .style("stroke", "transparent")
+                .attr("class", function(d){ return "Country" } )
+                .style("opacity", 1)
+                .on("mouseover", mouseOver)
+                .on("mousemove", mouseMove)
+                .on("mouseleave", mouseLeave);
         }
-
-        svg.append("g")
-            .selectAll("path")
-            .data(geoData.features)
-            .enter()
-            .append("path")
-            // draw each country
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            // set the color of each country
-            .attr("fill", function (d) {
-                d.total = data.get(d.id) || 0;
-                return colorScale(d.total);
-            })
-            .style("stroke", "transparent")
-            .attr("class", function(d){ return "Country" } )
-            .style("opacity", 1)
-            .on("mouseover", mouseOver)
-            .on("mousemove", mouseMove)
-            .on("mouseleave", mouseLeave);
-
-    }, [isDarkMode]);
+    }, [isDarkMode, geoDataStatus, geoData, popDataStatus, popData]);
 
     const graphContRef = useD3(renderFunc, null, false);
 
@@ -110,6 +125,8 @@ const ChoroplethMap = () => {
         <div className='map__wrapper'>
             <Typography sx={{fontSize: '24px', padding: '16px 0 20px 36px', fontWeight: '700', fontFamily: '"ABeeZee", sans-serif'}}>Choropleth Map</Typography>
             <Typography sx={{fontSize: '22px', padding: '20px', textAlign: 'center', fontWeight: '300', fontFamily: 'system-ui'}}>Country-wise population data (2004)</Typography>
+            {(geoDataStatus === 'pending' || popDataStatus === 'pending') && <LoadingComp loadingText={'Fetching data'}/>}
+            {(geoDataStatus === 'rejected' || popDataStatus === 'rejected') && <ErrorComp errorText={'Error fetching data'} />}
             <div ref={graphContRef} className='map__cont' style={{padding: '0 0'}}></div>
         </div>
     )
